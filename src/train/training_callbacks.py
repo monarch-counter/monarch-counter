cada1aed56fb9f71dd439cff71b21b26856ca81d
Shruti Patel <shruti222patel@gmail.com>
2021-10-11 22:14:24 -0400

    Add training_callbacks

---
+++ src/train/training_callbacks.py
@@ -0,0 +1,15 @@
+
+from tensorflow.keras.callbacks import Callback
+
+
+class LogToAzure(Callback):
+    '''Keras Callback for realtime logging to Azure'''
+
+    def __init__(self, run):
+        super(LogToAzure, self).__init__()
+        self.run = run
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Log all log data to Azure
+        for k, v in logs.items():
+            self.run.log(k, v)
